347276359c24bd40dc4c90cd86392893c30fa7b8
Вадим Залогин <vadiimreg73@outlook.com>
2023-06-28 22:28:40 +0400

    All exercises ready

--- Third-Exercise/3.js
+++ Third-Exercise/3.js
@@ -1,36 +1,18 @@
 
-
-// function groupByField(){
-//     const array = [{test: 1},{test: 2},{test: 3},{test: 1},{test: 1}];
-//     array.forEach(item => {
-//         arr[item] = (arr[item] || 0) + 1;
-//     });
-//     return arr;
-// }
-// arr = groupByField();
-
-// console.log(arr);
-
-
-function groupByDate() {
-    let arr = [{test: 1},{test: 2},{test: 3},{test: 1},{test: 1}];
-    //свертка массива во временный объект, с датами в качестве ключей
-    const temp = arr.reduce((acc, elem) => {
-      // делим строку по пробелу и извлекаем 0й элемент '2018-03-05 12:00' -> '2018-03-05'
-        const date = elem.date.split(' ')[0];
-      //если ключа-даты еще нет в объекте, записываем туда пустой массив
-        if(!acc[date]) {
-            acc[date] = [];
-            
-        }
-      // ложим текущий элемент в соответствующий массив
-        acc[date].push(elem);
-        return acc;
-        
-    }, {});
-    // извлекаем все ключи получившегося объекта в массив
-    // и преобразуем массив ключей в массив значений
-    return Object.getOwnPropertyNames(temp).map(k => temp[k]);
-    
+function groupByField(arr, field) {
+const groupedObj = {};
+for (let i = 0; i < arr.length; i++) {
+    const obj = arr[i];
+    const value = obj[field];
+    if (groupedObj[value]) {
+        groupedObj[value].push(obj);
+    } else {
+        groupedObj[value] = [obj];
+    }
+    }
+    return groupedObj;
 }
 
+const arr = [{test: 1},{test: 2},{test: 3},{test: 1},{test: 1}];
+const res = groupByField(arr, 'test');
+console.log(res);
